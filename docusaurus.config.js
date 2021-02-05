b8a538a44191a2702d94faa56a61148b87dcd795
Yutaka Catch <961566+ycatch@users.noreply.github.com>
2021-02-06 00:33:55 +0900

    Revert "Update baseUrl"
    This reverts commit a21fab5986f9d82c0755e78f91414d2a144b7825.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://ycatch.github.io/docusaurus/',
-  baseUrl: '/docusaurus/',
+  baseUrl: 'https://github.com/ycatch/docusaurus',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
