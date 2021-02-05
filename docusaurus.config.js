a21fab5986f9d82c0755e78f91414d2a144b7825
Yutaka Catch <961566+ycatch@users.noreply.github.com>
2021-02-05 23:53:24 +0900

    Update baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://ycatch.github.io/docusaurus/',
-  baseUrl: 'https://github.com/ycatch/docusaurus',
+  baseUrl: '/docusaurus/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
