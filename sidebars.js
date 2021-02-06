b01d97bac9dd2d9c21fdc80a6e0ad9356672f2a6
Yutaka Catch <961566+ycatch@users.noreply.github.com>
2021-02-07 01:18:36 +0900

    Revert "add pages"
    This reverts commit 3e4c17af8ea43e6447df6e8e601f535861bc0a24.

--- sidebars.js
+++ sidebars.js
@@ -1,13 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['list/doc1', 'list/doc2', 'list/doc3'],
-    catch_wiki: [
-      'tools/index', 
-      'tools/flatfile_cms', 
-      'tools/mdwiki',
-      'tools/mdwikidev', 
-      'tools/singularity', 
-      'tools/phaser_and_tiled'],
+    Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
   },
 };
